1bee95540e23d1c80de5602c03ae9e25fb0dd2c1
takuya0206 <tokiwatakuya@gmail.com>
2017-09-02 05:05:56 +0000

    HTMLでの結果表示

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -2,9 +2,16 @@
 const generator = document.forms.generator;
 const sentence = generator.sentence;
 const guide = document.getElementById('guide');
-
+const showExample= document.getElementById('showExample');
+const frame = document.getElementById('frame');
 
 sentence.addEventListener('input', () => {
   let restNum = 500 - sentence.value.length
   guide.textContent = 'ふりがなをつけたい文章（残り' + restNum + '字)';
+});
+
+showExample.addEventListener('click', () => {
+  let result = document.getElementById('result');
+  console.log(result.value);
+  frame.innerHTML = result.value;
 });
