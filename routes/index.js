5f7ea8f40d2b519ee09f1ac39155f8fc83b4f933
takuya0206 <tokiwatakuya@gmail.com>
2017-09-02 02:18:56 +0000

    アルファベット、数字への対応

--- routes/index.js
+++ routes/index.js
@@ -31,31 +31,33 @@ router.post('/generate', (req, res, next) => {
   request.post(options, (error, res, body) => {
     let word, subword = '';
     data.rubySentence = '';
-    xml2js.parseString(body.toString(), (err, array) => {
-      for (let i = 0, len = array.ResultSet.Result[0].WordList[0].Word.length; i < len; i++) {
-       word = array.ResultSet.Result[0].WordList[0].Word[i];
-       // 送り仮名がある場合の対応
+    xml2js.parseString(body, (err, callback) => {
+      for (let i = 0, len = callback.ResultSet.Result[0].WordList[0].Word.length; i < len; i++) {
+       word = callback.ResultSet.Result[0].WordList[0].Word[i];
+       // 数字やローマ字の場合
+       if (!word.hasOwnProperty('Furigana')) {
+          data.rubySentence += word.Surface;
+        };
+       // 助詞などのひらがなをスキップ
+       if (word.hasOwnProperty('Furigana') && word.Furigana[0] == word.Surface[0]) {
+        data.rubySentence += word.Surface;
+      };
+       // SubWord(=送り仮名)がある場合
        if (word.hasOwnProperty('SubWordList')) {
         for (let j = 0, len2 = word.SubWordList[0].SubWord.length; j < len2; j++) {
           subword = word.SubWordList[0].SubWord[j];
-          // ひらがなの振り仮名はスキップ
-          if (subword.Furigana[0] == subword.Surface[0] || subword.Furigana[0] == undefined ) {
+          if (subword.Furigana[0] == subword.Surface[0]) {
             data.rubySentence += subword.Surface;
-          } else {
-           data.rubySentence += ruby[0] + subword.Surface + ruby[1] + subword.Furigana + ruby[2];
-         };
+        } else {
+         data.rubySentence += ruby[0] + subword.Surface + ruby[1] + subword.Furigana + ruby[2];
        };
      };
-      // 送り仮名なしの場合
-      if (!word.hasOwnProperty('SubWordList')) {
-      // 念のため、ひらがなをケア
-      if (word.Furigana[0] == word.Surface[0] || word.Furigana[0] == undefined) {
-        data.rubySentence += word.Surface;
-      } else {
+   };
+      // 漢字だけの場合
+      if (word.hasOwnProperty('Furigana') && word.Furigana[0] !== word.Surface[0]) {
        data.rubySentence += ruby[0] + word.Surface + ruby[1] + word.Furigana + ruby[2];
      };
    };
- };
 });
   });
   let wait = setInterval (() => {
